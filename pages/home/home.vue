e86aabe87ff36901f366ff0b880fd6185d1f7e3e
lili <1124955977@qq.com>
2022-09-07 19:03:05 +0800

    完成了首页功能的开发

--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,19 +1,148 @@
 <template>
   <view>
-    home
+    <!-- 轮播图区域 -->
+    <swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000" :circular="true">
+      <swiper-item v-for="(item, i) in swiperList" :key="i">
+        <navigator class="swiper-item" :url="'/subpkg/goods_detail/goods_detail?goods_id=' + item.goods_id">
+          <image :src="item.image_src">
+        </navigator>
+      </swiper-item>
+    </swiper> 
+    <!-- 分类导航区域 -->
+    <view class="nav-list">
+       <view class="nav-item" v-for="(item, i) in navList" :key="i" @click="navClickHandler(item)">
+         <image :src="item.image_src" class="nav-img"></image>
+       </view>
+    </view>
+    <!-- 楼层区域 -->
+    <view class="floor-list">
+      <view class="floor-item" v-for="(item, i) in floorList" :key='i'>
+        <!-- 楼层标题 -->
+        <image :src="item.floor_title.image_src" class="floor-title"></image>
+        <!-- 楼层图片 -->
+        <view class="floor-img-box">
+             <!-- 左侧大图片盒子 -->
+          <navigator class="left-img-box" :url="item.product_list[0].url">
+           <image :src="item.product_list[0].image_src" :style="{width: item.product_list[0].image_width + 'rpx'}" mode="widthFix"></image>
+          </navigator>
+          <!-- 右侧小图片的盒子 -->
+          <view class="right-img-box">
+            <navigator class="right-img-item" v-for='(item2, i2) in item.product_list' :key='i2' v-if ='i2 !== 0' :url="item2.url">
+              <image :src="item2.image_src" mode="widthFix" :style="{width:item2.image_width + 'rpx'}"></image>
+            </navigator>
+          </view>
+        </view>
+      </view>
+      
+    </view>
+
   </view>
+
+
 </template>
 
 <script>
   export default {
     data() {
       return {
-        
+        // 轮播图数据列表
+        swiperList:[],
+        // 分类导航的数据列表
+        navList:[],
+        // 楼层的数据
+        floorList:[],
       };
+    },
+    onLoad() {
+      // 调用方法，获取轮播图数据
+      this.getSwiperList()
+      this.getNavList()
+      this.getFloorList()
+    },
+    methods:{
+      // 获取轮播图数据
+      async getSwiperList() {
+        const {data: res} = await uni.$http.get('/api/public/v1/home/swiperdata')
+        // 请求失败
+        if(res.meta.status !== 200) {
+          return uni.$showMsg()
+        }
+        // 请求成功
+        this.swiperList=res.message
+        uni.$showMsg('数据请求成功！')
+      },
+      // 获取导航栏数据
+      async getNavList() {
+        const {data: res} = await uni.$http.get('/api/public/v1/home/catitems')
+        // 请求失败
+        if(res.meta.status !== 200) {
+          return uni.$showMsg()
+        }
+        // 请求成功
+        this.navList=res.message
+        uni.$showMsg('数据请求成功！')
+      },
+     // 获取楼层数据
+     async getFloorList() {
+       const {data: res} = await uni.$http.get('/api/public/v1/home/floordata')
+       // 请求失败
+       if(res.meta.status !== 200) {
+         return uni.$showMsg()
+       }
+       // 对数据进行处理
+       res.message.forEach(floor => {
+         floor.product_list.forEach(prod => {
+           prod.url='/subpkg/goods_list/goods_list?' + prod.navigator_url.split('?'[1])
+         })
+       })
+       // 请求成功
+       this.floorList=res.message
+       uni.$showMsg('数据请求成功！')
+     },
+     
+     navClickHandler(item) {
+       // 判断点击的是哪个nav
+       if(item.name == '分类') {
+         uni.switchTab({
+           url:'/pages/cate/cate'
+         })
+       }
+     } 
+      
     }
   }
 </script>
 
 <style lang="scss">
+swiper {
+  height: 330rpx;
+  .swiper-item,
+    image{
+      width: 100%;
+      height:100%;
+}  }
+  .nav-list {
+    display: flex;
+    justify-content: space-around;
+    margin: 15px 0;
+    .nav-img {
+      width: 128rpx;
+      height: 140rpx;
+    }
+  }
+  .floor-title{
+    width: 100%;
+    height: 60rpx;
+  }
+.right-img-box {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+}
+
+.floor-img-box {
+  display: flex;
+  padding-left: 10rpx;
+}
 
 </style>
---
+++ subpkg/goods_list/goods_list.vue
@@ -0,0 +1,19 @@
+<template>
+  <view>
+    陈
+  </view>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        
+      };
+    }
+  }
+</script>
+
+<style lang="scss">
+
+</style>
